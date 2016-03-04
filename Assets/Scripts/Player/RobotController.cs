88f8b870bd1dba2f9c5e904b4c48d98d4ce14767
Julien Revel <Julien Revel>
2016-03-04 14:28:47 -0500

    Gestion du déplacement des personnages

--- Assets/Scripts/Player/RobotController.cs
+++ Assets/Scripts/Player/RobotController.cs
@@ -2,16 +2,30 @@
 using System.Collections;
 
 public class RobotController : MonoBehaviour {
+    public float maxSpeed, acceleration, deceleration, inputTrigger;
+    public int playerId;
+    public PlayerInputs input;
+
+    private Vector2 speed;
 
 	// Use this for initialization
 	void Start () {
+
 	}
 	
 	// Update is called once per frame
 	void Update ()
     {
-        print(InputManager.Instance.pilot[0].CameraX);
-
-
+        if(input == null)
+        {
+            input = InputManager.Instance.pilot[playerId];
+        }
+        Vector2 inputDir = new Vector2(input.Yaw, -input.Pitch);
+        speed += acceleration * inputDir * Time.deltaTime;
+        if (inputDir.magnitude < inputTrigger)
+            speed *= Mathf.Pow(deceleration, Time.deltaTime);
+        if (speed.magnitude > maxSpeed)
+            speed *= maxSpeed / speed.magnitude;
+        transform.position += new Vector3(speed.x, 0, speed.y) * Time.deltaTime;
     }
 }
