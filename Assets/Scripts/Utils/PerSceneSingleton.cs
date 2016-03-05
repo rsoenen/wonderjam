f132e83a6a616c70d22eec7ab0c2da22b068cf14
lamogui <julien.deloor@gmail.com>
2016-03-05 00:54:20 -0500

    fbreyogeryogyogyogyodgygygygyoggyo

--- Assets/Scripts/Utils/PerSceneSingleton.cs
+++ Assets/Scripts/Utils/PerSceneSingleton.cs
@@ -1,15 +1,58 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class PerSceneSingleton : MonoBehaviour {
+public class PerSceneSingleton<T> : MonoBehaviour where T : MonoBehaviour {
+	private static T _instance;
 
-	// Use this for initialization
-	void Start () {
-	
+	private static object _lock = new object();
+
+	public static T Instance
+	{
+		get
+		{
+			lock (_lock)
+			{
+				if (_instance == null)
+				{
+					_instance = (T)FindObjectOfType(typeof(T));
+
+					if (FindObjectsOfType(typeof(T)).Length > 1)
+					{
+						Debug.LogError("[Singleton] Something went really wrong " +
+							" - there should never be more than 1 singleton!" +
+							" Reopening the scene might fix it.");
+						return _instance;
+					}
+
+					if (_instance == null)
+					{
+						GameObject singleton = new GameObject();
+						_instance = singleton.AddComponent<T>();
+						singleton.name = "(singleton) " + typeof(T).ToString();
+
+					}
+					else
+					{
+						Debug.Log("[Singleton] Using instance already created: " +
+							_instance.gameObject.name);
+					}
+				}
+
+				return _instance;
+			}
+		}
 	}
-	
-	// Update is called once per frame
-	void Update () {
-	
+
+	/// <summary>
+	/// When Unity quits, it destroys objects in a random order.
+	/// In principle, a Singleton is only destroyed when application quits.
+	/// If any script calls Instance after it have been destroyed, 
+	///   it will create a buggy ghost object that will stay on the Editor scene
+	///   even after stopping playing the Application. Really bad!
+	/// So, this was made to be sure we're not creating that buggy ghost object.
+	/// </summary>
+	public void OnDestroy()
+	{
+		_instance = null;
 	}
 }
