6c5365d7c5c18ecef1dd3d3758aa4d3b75f742c1
Sharundaar <rabiller.donatien@hotmail.fr>
2016-03-06 01:13:40 -0500

    Added a script to beep and destroy after some existing time.

---
+++ Assets/Scripts/Utils/TimerAndDeletion.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TimerAndDeletion : MonoBehaviour {
+
+    [SerializeField]
+    private float m_DestructionETA;
+    private float m_timer;
+
+	// Update is called once per frame
+	void Update () {
+
+        m_timer += Time.deltaTime;
+        if(m_timer > m_DestructionETA)
+        {
+            BeepAndDestroy beepAndDestroy = gameObject.AddComponent<BeepAndDestroy>();
+            beepAndDestroy.Init(5, 0.2f, 2.0f);
+            Destroy(this);
+        }
+	
+	}
+}
