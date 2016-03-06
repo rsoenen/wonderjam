6be17ab6701aa68478bfb849b4d0d6c3fe922c28
Sharundaar <rabiller.donatien@hotmail.fr>
2016-03-05 21:35:27 -0500

    Added ThrowingBehavior Adapted the ArenaWater.unity colliders

---
+++ Assets/Scripts/ThrowableObject.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+class ThrowableObject : MonoBehaviour
+{
+    [SerializeField]
+    Rigidbody m_rigidbody;
+}
